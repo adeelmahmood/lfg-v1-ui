e602f3ffa3c7cb9bbb85766af1bb11e9a120be97
aqureshiest <adeelqureshi@M6TWYY7lQureshi.lan>
2023-01-13 20:05:04 -0500

    redirect debug on staging

--- pages/login.js
+++ pages/login.js
@@ -10,9 +10,9 @@ export default function Borrower() {
 
     const redirectURL = () => {
         let url =
-            process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-            process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-            "http://localhost:3000/";
+            process?.env?.NEXT_PUBLIC_OAUTH_REDIRECT_URL ?? // Manually set in vercel (mainly for staging)
+            process?.env?.NEXT_PUBLIC_SITE_URL ?? // This points to prod url
+            "http://localhost:3000/"; // local
         // Make sure to include `https://` when not localhost.
         url = url.includes("http") ? url : `https://${url}`;
         // Make sure to including trailing `/`.
