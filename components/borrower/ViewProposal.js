6e7db81f726ed04e911036f7a59412fcb9656062
aqureshiest <adeelqureshi@M6TWYY7lQureshi.lan>
2023-02-04 13:26:58 -0500

    working on view proposal page

--- components/borrower/ViewProposal.js
+++ components/borrower/ViewProposal.js
@@ -27,11 +27,11 @@ export default function ViewProposal({ loanProposal, ...rest }) {
 
     return (
         <>
-            <div className="mt-10 mb-10">
-                <h2 className="mb-4 text-left text-4xl font-bold uppercase tracking-tight text-gray-800 dark:text-gray-200 md:text-5xl md:tracking-wide">
+            <div className="mt-8 mb-10">
+                <h2 className="mb-2 text-left text-3xl font-bold uppercase tracking-tight text-gray-800 dark:text-gray-200 md:text-4xl md:tracking-wide">
                     Loan Proposal
                 </h2>
-                <h2 className="mb-8 text-left text-5xl font-bold tracking-tight text-white md:text-6xl md:tracking-wide">
+                <h2 className="mb-4 text-left text-5xl font-bold tracking-tight text-white md:text-6xl md:tracking-wide">
                     <span className="bg-gradient-to-r from-indigo-500 to-green-600 bg-clip-text uppercase text-transparent">
                         {getSelected(
                             loanProposal.business_title,
@@ -41,8 +41,8 @@ export default function ViewProposal({ loanProposal, ...rest }) {
                         )}
                     </span>
                 </h2>
-                <div className="flex flex-col justify-between md:flex-row">
-                    <div className="mb-6 flex items-center md:justify-center">
+                <div className="flex justify-between">
+                    <div className="mb-2 flex items-center md:justify-center">
                         <div className="block h-24 w-24 overflow-hidden rounded-full border-2 border-indigo-400 hover:shadow-md focus:outline-none">
                             <img
                                 src={user?.user_metadata.avatar_url}
@@ -59,18 +59,18 @@ export default function ViewProposal({ loanProposal, ...rest }) {
                             </span>
                         </div>
                     </div>
-                    <div className="flex flex-col items-start space-y-2 md:items-end">
+                    <div className="flex flex-col items-end space-y-2">
                         <button className="btn-clear text-base">
-                            <HandThumbUpIcon className="mr-2 inline h-6 fill-current align-top text-gray-800 dark:text-gray-200" />
-                            Like this Proposal
+                            <HandThumbUpIcon className="inline h-6 fill-current align-top text-gray-800 dark:text-gray-200" />
+                            <span className="ml-2 hidden md:inline">Like this Proposal</span>
                         </button>
                         <button className="btn-primary text-base">
-                            <EnvelopeIcon className="mr-2 inline h-6 fill-current align-top text-white dark:text-gray-800" />
-                            Vote on this Proposal
+                            <EnvelopeIcon className="inline h-6 fill-current align-top text-white dark:text-gray-800" />
+                            <span className="ml-2 hidden md:inline">Vote on this Proposal</span>
                         </button>
                     </div>
                 </div>
-                <div className="relative mt-2 pb-2/3 shadow-lg">
+                <div className="relative mt-4 pb-2/3 shadow-lg">
                     <img
                         className="absolute h-full w-full rounded-xl object-cover object-center"
                         src={loanProposal.banner_image}
